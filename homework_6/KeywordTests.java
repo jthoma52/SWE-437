f781ddea47b7488c2226cd3c4d490624bdab9dc2
jasont566 <Jason.t566@live.com>
2018-02-28 12:45:21 -0500

    Added make keytest and set up KeywordTests file

---
+++ homework_6/KeywordTests.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+import java.io.*;
+
+public class KeywordTests {
+
+	public static void main(String[] args) {
+		org.junit.runner.JUnitCore.main("KeywordTests");
+	}
+
+	@Test
+	public void firstCommit() {
+		assertTrue(true);
+	}
+
+}
